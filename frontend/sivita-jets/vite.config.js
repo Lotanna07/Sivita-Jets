d0518d8eaa351559dee6b2d35951bb6ce726dd8a
lotanna <lotanna006@gmail.com>
2026-07-15 18:41:05 +0100

    update

--- frontend/sivita-jets/vite.config.js
+++ frontend/sivita-jets/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Sivita-Jets/',
   plugins: [react(), tailwindcss()],
+  base: '/Sivita-Jets/',
+  
 })
